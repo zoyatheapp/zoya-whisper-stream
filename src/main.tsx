01590ac81316a3b52f6823a0d8443159f69bdce7
zoyatheapp <ghaniadanish76@gmail.com>
2025-08-26 14:34:30 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ensureWebRTCGlobals } from '@/lib/webrtc'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ensureWebRTCGlobals();
+createRoot(document.getElementById('root')!).render(<App />);
