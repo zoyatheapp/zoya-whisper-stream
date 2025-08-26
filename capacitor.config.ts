1db5ca9f558c33c586b2202b92e5aee9a4d5c123
ghania <ghaniadanish@icloud.com>
2025-08-26 11:12:15 +0300

    Add iosrtc plugin for WebRTC support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.app.zoya',
+  appName: 'zoya',
+  webDir: 'dist'
+};
+
+export default config;
